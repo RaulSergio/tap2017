f5783094460fc1ebc57c8be0dd1996a393f6f9fa
RaulSergio <dragolaymann@gmail.com>
2017-03-13 22:17:42 -0400

    exclui e altera

--- sistema-usuarios/src/main/java/controller/UsuarioController.java
+++ sistema-usuarios/src/main/java/controller/UsuarioController.java
@@ -38,6 +38,44 @@ protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws S
 }
 
 @Override
+protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {	
+	
+	/*String nome = req.getParameter("nome");
+	String email = req.getParameter("email");
+	
+	
+	for(int i=0;i<lista.size();i++){
+		if(lista.get(i).getEmail().equals(email) && lista.get(i).getNome().equals(nome)){
+			lista.remove(i);
+		}
+		
+	}*/
+	
+	//deletar por indice
+	int ind = Integer.parseInt(req.getParameter("indice"));
+		lista.remove(ind);
+	
+}
+
+@Override
+protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	int indice = Integer.parseInt(req.getParameter("indice"));
+	
+	String nome = req.getParameter("nome");
+	String email = req.getParameter("email");
+	
+	if(nome != "" && email.equals("")){
+		lista.get(indice).setNome(nome);
+	}else if(nome.equals("") && email != ""){
+		lista.get(indice).setEmail(email);
+	}else if(nome != "" && email != ""){
+		lista.get(indice).setNome(nome);
+		lista.get(indice).setEmail(email);
+	}
+	
+}
+
+@Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	String json = "[";
 	for (int i = 0; i < lista.size(); i++) {
--- sistema-usuarios/src/main/java/model/Usuario.java
+++ sistema-usuarios/src/main/java/model/Usuario.java
@@ -4,6 +4,8 @@ public class Usuario {
 	private String nome,email;
 	
 	
+	public Usuario() {	}
+	
 	public Usuario(String nome, String email){
 		this.nome = nome;
 		this.email = email;
